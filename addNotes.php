05ff1af5dc851835d13b8546eba266979c4599a4
Tom Felton <666@2810101.ru>
2021-03-16 17:57:32 +0700

    Получаем комментарии и добавляем в срм

---
+++ addNotes.php
@@ -0,0 +1,57 @@
+<?php
+function addNotes($auru,$rr,$subdomain){
+    $text = array(array(
+        "entity_id"=>$rr,
+        "note_type"=> "common",
+        "params"=>array(
+            "text" => $auru->itemName." ".$auru->quest,
+        )
+    )
+    );
+    $link = 'https://' . $subdomain . '.amocrm.ru/api/v4/leads/notes';
+    $curl=curl_init(); #Сохраняем дескриптор сеанса cURL
+    #Устанавливаем необходимые опции для сеанса cURL
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_USERAGENT, 'amoCRM-API-client/1.0');
+    curl_setopt($curl, CURLOPT_URL, $link);
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($text));
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    curl_setopt($curl, CURLOPT_COOKIEFILE, dirname(__FILE__) . '/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+    curl_setopt($curl, CURLOPT_COOKIEJAR, dirname(__FILE__) . '/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+    $out = curl_exec($curl); #Инициируем запрос к API и сохраняем ответ в переменную
+    $notes = json_decode($out, false);
+    $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    getError($code);
+    return $rr;
+}
+//// обработчик ошибок amoCRM
+//    function getError($code)
+//    {
+//        /* Теперь мы можем обработать ответ, полученный от сервера. Это пример. Вы можете обработать данные своим способом. */
+//        $code=(int)$code;
+//        $errors=array(
+//            301=>'Moved permanently',
+//            400=>'Bad request',
+//            401=>'Unauthorized',
+//            403=>'Forbidden',
+//            404=>'Not found',
+//            500=>'Internal server error',
+//            502=>'Bad gateway',
+//            503=>'Service unavailable'
+//        );
+//        try
+//        {
+//            /* Если код ответа не равен 200 или 204 - возвращаем сообщение об ошибке */
+//            if($code!=200 && $code!=204) {
+//                throw new Exception(isset($errors[$code]) ? $errors[$code] : 'Undescribed error',$code);
+//            }
+//        }
+//        catch(Exception $E)
+//        {
+//            die('Ошибка: '.$E->getMessage().PHP_EOL.'Код ошибки: '.$E->getCode());
+//        }
+//    }
--- au.php
+++ au.php
@@ -1,4 +1,5 @@
 <?php
+require_once('dobavitvamocrm.php');
 //e4dt27aVuDKLQPJ0szOtagjSFqILJ5xc
 //https://market-api.au.ru/v1/auth/token
 //$url = 'https://au.ru/login/?returnUrl=https%3A%2F%2Fau.ru%2F';
@@ -9,11 +10,25 @@
 /// {
 //    "token": "MSmbpE0Qom66k1f67QuewJ9lA7uvhIAg" //Аторизационный токен который необходимо передавать в заголовке X-Auth-Token
 //}
-function curlLogin(){
+
+$host_db= 'localhost';
+$user_db = 'u0605_u0605727';
+$password_db = '84kf_Ll3';
+$db = 'u0605727_auamoapi';
+$mysqli = new mysqli($host_db, $user_db, $password_db, $db);
+if($mysqli->connect_errno){
+    file_put_contents('db_errors.txt', "(".$mysqli->connect_errno.") ".$mysqli->connect_error);
+    echo "(".$mysqli->connect_errno.") ".$mysqli->connect_error;
+}
+$mysqli->query("SET NAMES 'utf8'");
+curlLogin($mysqli);
+function curlLogin($mysqli){
     $url = 'https://market-api.au.ru/v1/auth/token/';
+    $subdomain = 'emomalisharifov98yandexru';
     $params = array(
         'login' => 'ТЕМАСФО',
         'password' => '123456Qwe',
+        'token' =>'e4dt27aVuDKLQPJ0szOtagjSFqILJ5xc',
     );
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
@@ -21,19 +36,78 @@ function curlLogin(){
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
-    // Или предать массив строкой:
-    // curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($array, '', '&'));
-
     curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookie.txt');
     curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt');
-
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-
     $result = curl_exec($ch);
+    $rr = json_decode($result, false);
+//    print_r($rr->token);
+    $url = 'https://market-api.au.ru/v1/questions/unanswered/';
+    $headers = array(
+        "X-Auth-Token: $rr->token",
+        "Content-Type: application/json",
+    );
+    $ch = curl_init(); //<- вот тут новый адрес
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookie.txt');
+    curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt');
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    $result = curl_exec($ch);
+    $rr = json_decode($result, false);
     curl_close($ch);
+    echo '<pre>';
+    $rrr = (array_reverse($rr));
+    $monthStart = date('Y-m-01');
+    for($i = 0; $i < count($rrr); ++$i) {
+        if($monthStart <= $rrr[$i]->dateCreate) {
+            echo '<pre>';
+            echo $id = $rrr[$i]->id;
+            echo '<pre>';
+            echo $lotId = $rrr[$i]->lotId;
+            echo '<pre>';
+            echo $userId = $rrr[$i]->userId;
+            echo '<pre>';
+            echo $quest = $rrr[$i]->quest;
+            echo '<pre>';
+            echo $dateCreate = $rrr[$i]->dateCreate;
+            echo '<pre>';
+            echo $login = $rrr[$i]->login;
+            echo '<pre>';
+            echo $itemName = $rrr[$i]->itemName;
+            echo '<pre>';
+            echo $questionUrl = $rrr[$i]->questionUrl;
+            echo '<pre>';
+            var_dump(postaddNotes($rrr[$i],$subdomain));
 
-    echo $result;
+        }
+
+
+//        $query = "INSERT INTO `leads` (id, lotId, userId, quest, dateCreate, login, itemName, questionUrl, id_maneger, answer) VALUES
+//        (
+//         '$id',
+//         '$rrr[$i]->lotId',
+//         '$rrr[$i]->userId',
+//         '$rrr[$i]->quest',
+//         '$rrr[$i]->dateCreate',
+//         '$rrr[$i]->login',
+//         '$rrr[$i]->itemName',
+//         '$rrr[$i]->questionUrl',
+//         '0',
+//         'answer')";
+//        $mysqli->query($query);
+//        echo $mysqli->affected_rows;
+//        echo $query;
+//        echo $mysqli->error;
+    }
+    return $rr;
+
 
 }
-curlLogin();
+
+
+
+?>
+
 
