338a737a8262d89974cad1742ec9696d496af9ac
Tom Felton <666@2810101.ru>
2021-04-02 11:33:21 +0700

    ФИнал финал точно финал....

--- otvetNotes.php
+++ otvetNotes.php
@@ -9,13 +9,18 @@ function logger($msg, $prefix='')
     $account_idmanagera = (int)($msg['leads']['note'][0]['note']['account_id']);
     $idmenegera = (int)($msg['leads']['note'][0]['note']['created_by']);
     $idvAU = (int)(substr(($msg['leads']['update'][0]['name']),-8));
-    $leads = $msg['lead'];
+    $leads = $msg['leads'];
+    $notes = $msg['notes'];
+
     file_put_contents('log_'.$prefix.$name.'_'.date('md').'.txt', print_r( $ovetmeneger,true).PHP_EOL, FILE_APPEND);
     file_put_contents('log_'.$prefix.$name.'_'.date('md').'.txt', print_r( $idsdelki,true).PHP_EOL, FILE_APPEND);
     file_put_contents('log_'.$prefix.$name.'_'.date('md').'.txt', print_r( $account_idmanagera,true).PHP_EOL, FILE_APPEND);
     file_put_contents('log_'.$prefix.$name.'_'.date('md').'.txt', print_r( $idmenegera,true).PHP_EOL, FILE_APPEND);
     file_put_contents('log_'.$prefix.$name.'_'.date('md').'.txt', print_r( $idvAU,true).PHP_EOL, FILE_APPEND);
     file_put_contents('log_'.$prefix.$name.'_'.date('md').'.txt', print_r( $leads,true).PHP_EOL, FILE_APPEND);
+    file_put_contents('log_'.$prefix.$name.'_'.date('md').'.txt', print_r( $notes,true).PHP_EOL, FILE_APPEND);
+    file_put_contents('log_'.$prefix.$name.'_'.date('md').'.txt', print_r( $msg,true).PHP_EOL, FILE_APPEND);
+
     $subdomain = 'testtset122';
     $login = 'testtset122@yandex.ru';
     $hash = 'e0c8ac87c24805e334cf7e786533806d71bed262';
@@ -62,8 +67,11 @@ function logger($msg, $prefix='')
     curl_close($curl); #Завершаем сеанс cURL
     file_put_contents('log_'.$prefix.$name.'_'.date('md').'.txt', print_r( $leads,true).PHP_EOL, FILE_APPEND);
     $idvAU = (int)(substr($leads->name, -8));
-    file_put_contents('log_'.$prefix.$name.'_'.date('md').'.txt', print_r( $idvAU,true).PHP_EOL, FILE_APPEND);
-    curlLogin($idvAU,$ovetmeneger);
+    if($idmenegera == 6912532){
+        file_put_contents('log_'.$prefix.$name.'_'.date('md').'.txt', print_r( $idvAU,true).PHP_EOL, FILE_APPEND);
+        file_put_contents('log_'.$prefix.$name.'_'.date('md').'.txt', print_r( $idmenegera,true).PHP_EOL, FILE_APPEND);
+        curlLogin($idvAU,$ovetmeneger,$idmenegera);
+    }
 }
 $ovetmeneger = (string)($_POST['leads']['note'][0]['note']['text']);
 $idsdelki = (int)($_POST['leads']['note'][0]['note']['element_id']);
@@ -87,55 +95,58 @@ $idvAU = (int)(substr(($_POST['leads']['update'][0]['name']), -8));
 /// {
 //    "token": "MSmbpE0Qom66k1f67QuewJ9lA7uvhIAg" //Аторизационный токен который необходимо передавать в заголовке X-Auth-Token
 //}
-function curlLogin($idvAU,$ovetmeneger){
-    $prefix='';
-    $url = 'https://market-api.au.ru/v1/auth/token/';
-    $params = array(
-        'login' => 'ТЕМАСФО',
-        'password' => '123456Qwe',
-        'token' =>'e4dt27aVuDKLQPJ0szOtagjSFqILJ5xc',
-    );
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
-    curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookie.txt');
-    curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt');
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    $result = curl_exec($ch);
-    $rr = json_decode($result, false);
-    file_put_contents('log_'.$prefix.$name.'_'.date('md').'.txt', print_r( $rr,true).PHP_EOL, FILE_APPEND);
+function curlLogin($idvAU,$ovetmeneger,$idmenegera)
+{
+    if ($idmenegera == 6912532) {
+        $prefix = '';
+        $url = 'https://market-api.au.ru/v1/auth/token/';
+        $params = array(
+            'login' => 'ТЕМАСФО',
+            'password' => '123456Qwe',
+            'token' => 'e4dt27aVuDKLQPJ0szOtagjSFqILJ5xc',
+        );
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+        curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookie.txt');
+        curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt');
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        $result = curl_exec($ch);
+        $rr = json_decode($result, false);
+        file_put_contents('log_' . $prefix . $name . '_' . date('md') . '.txt', print_r($rr, true) . PHP_EOL, FILE_APPEND);
 //    print_r($rr->token);
-    $url = 'https://market-api.au.ru/v1/questions/answer/';
-    $text = array(
-        "questionId"=> $idvAU,
-        "answer"=> $ovetmeneger,
-    );
-    $headers = array(
-        "X-Auth-Token: $rr->token",
-        "Content-Type: application/json",
-    );
-    var_dump(json_encode($text));
-    $curl=curl_init(); #Сохраняем дескриптор сеанса cURL
-    #Устанавливаем необходимые опции для сеанса cURL
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_USERAGENT, 'amoCRM-API-client/1.0');
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
-    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($text));
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($curl, CURLOPT_COOKIEFILE, dirname(__FILE__) . '/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
-    curl_setopt($curl, CURLOPT_COOKIEJAR, dirname(__FILE__) . '/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
-    $out = curl_exec($curl); #Инициируем запрос к API и сохраняем ответ в переменную
-    $rr = json_decode($out, false);
-    $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-    file_put_contents('log_'.$prefix.$name.'_'.date('md').'.txt', print_r( $rr,true).PHP_EOL, FILE_APPEND);
-    file_put_contents('log_'.$prefix.$name.'_'.date('md').'.txt', print_r( $code,true).PHP_EOL, FILE_APPEND);
-    return $rr;
+        $url = 'https://market-api.au.ru/v1/questions/answer/';
+        $text = array(
+            "questionId" => $idvAU,
+            "answer" => $ovetmeneger,
+        );
+        $headers = array(
+            "X-Auth-Token: $rr->token",
+            "Content-Type: application/json",
+        );
+        var_dump(json_encode($text));
+        $curl = curl_init(); #Сохраняем дескриптор сеанса cURL
+        #Устанавливаем необходимые опции для сеанса cURL
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_USERAGENT, 'amoCRM-API-client/1.0');
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($text));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_COOKIEFILE, dirname(__FILE__) . '/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+        curl_setopt($curl, CURLOPT_COOKIEJAR, dirname(__FILE__) . '/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+        $out = curl_exec($curl); #Инициируем запрос к API и сохраняем ответ в переменную
+        $rr = json_decode($out, false);
+        $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        file_put_contents('log_' . $prefix . $name . '_' . date('md') . '.txt', print_r($rr, true) . PHP_EOL, FILE_APPEND);
+        file_put_contents('log_' . $prefix . $name . '_' . date('md') . '.txt', print_r($code, true) . PHP_EOL, FILE_APPEND);
+        return $rr;
 
+    }
 }
 ?>
--- test.php
+++ test.php
@@ -2,42 +2,6 @@
 require __DIR__ . '/vendor/autoload.php';
 $url = 'https://market-api.au.ru/v1/auth/token/';
 $subdomain = 'emomalisharifov98yandexru';
-$params = array(
-    'login' => 'ТЕМАСФО',
-    'password' => '123456Qwe',
-    'token' =>'e4dt27aVuDKLQPJ0szOtagjSFqILJ5xc',
-);
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
-curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookie.txt');
-curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt');
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-$result = curl_exec($ch);
-$rr = json_decode($result, false);
-//    print_r($rr->token);
-$url = 'https://market-api.au.ru/v1/questions/unanswered/';
-$headers = array(
-    "X-Auth-Token: $rr->token",
-    "Content-Type: application/json",
-);
-$ch = curl_init(); //<- вот тут новый адрес
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookie.txt');
-curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt');
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-$result = curl_exec($ch);
-$rr = json_decode($result, false);
-curl_close($ch);
-echo '<pre>';
-$rrr = (array_reverse($rr));
-$monthStart = '2021-03-01T00:00:00.087Z';
-for($i = 0; $i < 2; ++$i) {
     try {
         // Создание клиента
         $subdomain = 'testtset122';            // Поддомен в амо срм
@@ -45,24 +9,18 @@ for($i = 0; $i < 2; ++$i) {
         $apikey = 'e0c8ac87c24805e334cf7e786533806d71bed262';       // api ключ
         $amo = new \AmoCRM\Client($subdomain, $login, $apikey);
 
+        var_dump("lotId уже есть");
         $parameters = [
             'type' => 'lead',
-            'element_id' => 2738825
+            'element_id' => 2831699
         ];
         $notes = $amo->note;
         $idnotes = $notes->apiList($parameters);
+        echo '<pre>';
         print_r($idnotes);
-        echo '<pre>';
-        $data = (substr($idnotes[1]['text'], -24));
-        print_r($data);
-        echo '<pre>';
-        print_r($idnotes[1]['text']);
+        print_r("id v notes->apiList");
+        print_r(count($idnotes));
 
     } catch (\AmoCRM\Exception $e) {
         printf('Error (%d): %s' . PHP_EOL, $e->getCode(), $e->getMessage());
     }
-    if ($rrr[$i]->dateCreate==$data){
-        var_dump("dateCreate");
-    }
-
-}
